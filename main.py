55cdc3c2ae24156b39a4be481768f789663eb047
Robin Kunath <robinkunath.1@web.de>
2022-11-22 19:23:21 +0100

    Added Queue handling

--- main.py
+++ main.py
@@ -9,4 +9,8 @@ client = discord.ext.commands.Bot(command_prefix='!', intents = discord.Intents.
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
+    @client.event
+    async def on_ready():
+        await client.change_presence(activity=discord. Activity(type=discord.ActivityType.listening, name='!help'))
+
 client.run("MTA0MzkyNjc5NzgzMTY0MzE3OQ.GprBtv.HtwT4bRfaFo6v6mAeta6JPStNZIR_nj2hALKgY")
--- music.py
+++ music.py
@@ -2,6 +2,12 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
+queueList = []
+titleList = []
+currentTrack = 0
+
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+YDL_OPTIONS = {'format':"bestaudio"}
 class music(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -23,18 +29,34 @@ class music(commands.Cog):
         @client.command(name="play")
         async def play(ctx, url):
             ctx.voice_client.stop()
-            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-            YDL_OPTIONS = {'format':"bestaudio"}
             vc = ctx.voice_client
-
+   
             with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
                 info = ydl.extract_info(url, download=False)
                 url2 = info['formats'][0]['url']
                 titleInfo = info['title']
+                currentTrack = titleInfo
                 duration = convert(info['duration'])
                 source = discord.FFmpegPCMAudio(url2, **FFMPEG_OPTIONS, executable="C:\\ffmpeg\\bin\\ffmpeg.exe")
                 await ctx.reply(f"🎶Wird gespielt🎶\n**{titleInfo}** ({duration})", mention_author=False)
-                vc.play(source)
+                vc.play(source, after=lambda e: play_next(ctx))
+
+        @client.command(name="queue")
+        async def queue(ctx, url):
+            if ctx.author.voice is None:
+                await ctx.send("Du musst in einem Channel sein um die Queue zu bearbeiten")
+            else:
+                queueList.append(url)
+                YDL_OPTIONS = {'format':"bestaudio"}
+                with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                    info = ydl.extract_info(url, download=False)
+                    titleInfo = info['title']
+                    titleList.append(titleInfo)
+                    await ctx.reply(f"**{titleInfo}** der Warteschlange hinzugefügt", mention_author=False)
+
+        @client.command(name="listQueue")
+        async def listQueue(ctx):
+            await ctx.reply("In der Queue befinden sich im Moment: \n" + '\n'.join(map(str, titleList)), mention_author=False)        
 
         @client.command(name="pause")
         async def pause(ctx):
@@ -46,6 +68,16 @@ class music(commands.Cog):
             ctx.voice_client.resume()
             await ctx.reply("Es geht weiter⏩", mention_author=False)
 
+        @client.command(name="skip")
+        async def skip(ctx):
+            ctx.voice_client.stop()
+            play_next(ctx)
+
+        @client.command(name="stop")
+        async def stop(ctx):
+            clearQueue()
+            await leave(ctx)
+
 def setup(client):
     client.add_cog(music(client))
 
@@ -55,3 +87,23 @@ def convert(seconds):
     minutes = seconds // 60
     seconds %= 60
     return "%02d:%02d" % (minutes, seconds)
+
+def play_next(ctx):
+    vc = ctx.voice_client
+    if len(queueList) > 0:
+        url = getQueue()
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            currentTrack = info['title']
+            url2 = info['formats'][0]['url']
+            source = discord.FFmpegPCMAudio(url2, **FFMPEG_OPTIONS, executable="C:\\ffmpeg\\bin\\ffmpeg.exe")
+            vc.play(source, after=lambda e: play_next(ctx))
+
+def getQueue():
+    url = queueList.pop(0)
+    titleList.pop(0)
+    return url
+
+def clearQueue():
+    queueList.clear
+    titleList.clear
